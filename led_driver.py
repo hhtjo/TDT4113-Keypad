de625a31b812f7c93b2ec18e1cfcd21f36f57b75
Hans Tjøtta <hahotjo@gmail.com>
2021-03-08 10:32:06 +0100

    fix pylint

--- led_driver.py
+++ led_driver.py
@@ -1,8 +1,10 @@
+"""Module containing Led Driver"""
 from typing import Iterable
 from time import sleep
 from GPIOSimulator_v5 import GPIOSimulator
 
 class LedDriver():
+    """Led driver class"""
     def __init__(self) -> None:
         self.gpio_sim = GPIOSimulator()
         self.led_lighting_states = (
@@ -34,17 +36,18 @@ class LedDriver():
                 self._light_diode(led)
         sleep(0.00001)
 
-    def _light_simult(self, sequence:list, frame_count):
+    def _light_simult(self, sequence: list, frame_count):
         i = 0
         while i < frame_count:
             self._light_sequence(sequence)
             i += 1
 
-    def _light_anim(self, sequence:Iterable, frame_count:int=30):
+    def _light_anim(self, sequence: Iterable, frame_count: int = 30):
         for frame in sequence:
             self._light_simult(frame, frame_count)
 
     def power_up(self):
+        """Light power up sequence"""
         self._light_anim((
             (1, 0, 0, 0, 0, 0),
             (1, 1, 0, 0, 0, 0),
@@ -55,6 +58,7 @@ class LedDriver():
         ))
 
     def power_down(self):
+        """Light power down sequence"""
         self._light_anim((
             (1, 1, 1, 1, 1, 1),
             (1, 1, 1, 1, 1, 0),
@@ -65,6 +69,7 @@ class LedDriver():
         ))
 
     def twinkle(self):
+        """Light twinkle sequence"""
         self._light_anim((
             (0, 0, 0, 1, 1, 0),
             (0, 0, 1, 0, 0, 1),
@@ -81,22 +86,24 @@ class LedDriver():
         ), 10)
 
     def flash(self):
+        """Light flash sequence"""
         self._light_anim((
-            (1,1,1,1,1,1),
-            (0,0,0,0,0,0),
-            (1,1,1,1,1,1),
-            (0,0,0,0,0,0),
-            (1,1,1,1,1,1),
-            (0,0,0,0,0,0),
-            (1,1,1,1,1,1),
-            (0,0,0,0,0,0),
-            (1,1,1,1,1,1),
-            (0,0,0,0,0,0),
-            (1,1,1,1,1,1),
-            (0,0,0,0,0,0),
+            (1, 1, 1, 1, 1, 1),
+            (0, 0, 0, 0, 0, 0),
+            (1, 1, 1, 1, 1, 1),
+            (0, 0, 0, 0, 0, 0),
+            (1, 1, 1, 1, 1, 1),
+            (0, 0, 0, 0, 0, 0),
+            (1, 1, 1, 1, 1, 1),
+            (0, 0, 0, 0, 0, 0),
+            (1, 1, 1, 1, 1, 1),
+            (0, 0, 0, 0, 0, 0),
+            (1, 1, 1, 1, 1, 1),
+            (0, 0, 0, 0, 0, 0),
         ), 10)
 
-    def light_single(self, led:int, dur:float):
+    def light_single(self, led: int, dur: float):
+        """Light single LED"""
         self._light_diode(led)
         sleep(dur)
         self._all_to_input()
@@ -104,6 +111,7 @@ class LedDriver():
 
 
 def main():
+    """main func"""
     ld_driver = LedDriver()
     ld_driver.light_single(2, 1)
 
